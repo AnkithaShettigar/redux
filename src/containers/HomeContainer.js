e7e225bc8f471d70161acf84ebeaaa0a183f9bbe
AnkithaShettigar <ankitha.yermal@gmail.com>
2023-09-02 00:01:21 +0530

    container

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,12 @@
 import Home from '../component/Home';
 
-export default Home;
+import { connect } from 'react-redux';
+import { addToCart } from '../services/Actions/Action';
+
+const mapTOStateProps = (state) => ({});
+const mapToDispatchProps = (dispatch) => ({
+  addToCartHandler: (data) => dispatch(addToCart(data)),
+});
+
+export default connect(mapTOStateProps, mapToDispatchProps)(Home);
+// export default Home;
